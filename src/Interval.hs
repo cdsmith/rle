6799783fa3b66c1e4b29c91cd9033ebb51a851f8
Chris Smith <cdsmith@gmail.com>
2022-04-28 08:17:28 -0400

    Foldable for interval

--- src/Interval.hs
+++ src/Interval.hs
@@ -1,28 +1,51 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Interval (Interval) where
 
 import Compression (Compression (..))
 import Data.These (These (..))
 
-data Interval a = Interval a a deriving (Eq, Ord, Show)
+data Interval a where
+  Interval :: Enum a => Int -> Int -> Interval a
+  Unique :: a -> Interval a
+
+deriving instance Eq a => Eq (Interval a)
+
+deriving instance Ord a => Ord (Interval a)
+
+deriving instance Show a => Show (Interval a)
+
+instance Foldable Interval where
+  foldMap f (Interval i j) = foldMap (f . toEnum) [i .. j]
+  foldMap f (Unique x) = f x
 
 instance (Eq a, Enum a) => Compression Interval a where
-  logicalLength (Interval a b) = fromEnum b - fromEnum a + 1
+  logicalLength (Interval i j) = i - j + 1
+  logicalLength (Unique _) = 1
 
-  solo x = Interval x x
+  solo x
+    | x == toEnum i = Interval i i
+    | otherwise = Unique x
+    where
+      i = fromEnum x
 
-  popHead (Interval a b) =
-    (a, if a == b then Nothing else Just (Interval (succ a) b))
+  popHead (Interval i j) =
+    (toEnum i, if i == j then Nothing else Just (Interval (i + 1) j))
+  popHead (Unique x) = (x, Nothing)
 
-  popTail (Interval a b) =
-    (if a == b then Nothing else Just (Interval a (pred b)), b)
+  popTail (Interval i j) =
+    (if i == j then Nothing else Just (Interval i (j - 1)), toEnum j)
+  popTail (Unique x) = (Nothing, x)
 
-  tryConcat (Interval a b) (Interval c d) =
-    if succ b == c then Just (Interval a d) else Nothing
+  tryConcat (Interval i j) (Interval k l) =
+    if j + 1 == k then Just (Interval i l) else Nothing
+  tryConcat _ _ = Nothing
 
-  trySplit int@(Interval a b) i
+  trySplit int@(Interval j k) i
     | i <= 0 = That int
-    | i >= logicalLength int = This int
-    | otherwise = let mid = toEnum (fromEnum a + i) in These (Interval a (pred mid)) (Interval mid b)
+    | i > k - j = This int
+    | otherwise = let m = j + i in These (Interval i (m - 1)) (Interval m k)
+  trySplit int@(Unique _) i = if i <= 0 then That int else This int
