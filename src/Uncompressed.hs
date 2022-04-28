918c8c0a1c72f52dde65153fdde3ce098c7ad0d6
Chris Smith <cdsmith@gmail.com>
2022-04-28 08:03:34 -0400

    Add uncompressed scheme

---
+++ src/Uncompressed.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Uncompressed (Uncompressed) where
+
+import Compression (Compression (..))
+import Data.These (These (..))
+
+newtype Uncompressed a = Uncompressed a deriving (Eq, Ord, Show)
+
+instance Foldable Uncompressed where
+  foldMap f (Uncompressed x) = f x
+
+instance Compression Uncompressed a where
+  logicalLength _ = 1
+
+  solo = Uncompressed
+
+  popHead (Uncompressed x) = (x, Nothing)
+  popTail (Uncompressed x) = (Nothing, x)
+
+  tryConcat _ _ = Nothing
+
+  trySplit x i
+    | i <= 0 = That x
+    | otherwise = This x
